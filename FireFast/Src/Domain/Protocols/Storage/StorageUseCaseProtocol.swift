cc0cc95c14ab74c7ec3edd346e29a0ec3892db89
msoleimani <msoleimani@cognibox.com>
2021-09-04 15:00:43 -0400

    [FIX] domain protocol

--- FireFast/Src/Domain/Protocols/Storage/StorageUseCaseProtocol.swift
+++ FireFast/Src/Domain/Protocols/Storage/StorageUseCaseProtocol.swift
@@ -5,10 +5,10 @@
 //  Created by Behrad Kazemi on 4/5/21.
 //
 
-import Foundation
+import FirebaseStorage
 
 public protocol StorageUseCaseProtocol {
-  func upload(data: Data, path: String, contentType: StorageContentType?, onSuccess: @escaping (UploadInfoModel) -> Void, progressCompleted: ((Double) -> Void)?, onError: ((Error) -> Void)?)
+  func upload(data: Data, path: String, contentType: StorageContentType?, onSuccess: @escaping (UploadInfoModel) -> Void, progressCompleted: ((Double) -> Void)?, onError: ((Error) -> Void)?) -> StorageUploadTask
   func makeURL(path: String, onSuccess: @escaping (URL) -> Void, onError: ((Error) -> Void)?)
   func delete(path: String, completion: ((Error?) -> Void)?)
 }
