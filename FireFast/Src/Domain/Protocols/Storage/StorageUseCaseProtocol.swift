b752949373def7364006bd27e450b7b1b73c0877
msoleimani <msoleimani@cognibox.com>
2021-09-04 14:32:06 -0400

    [UPDATE] the upload to storage and add content type and progress

--- FireFast/Src/Domain/Protocols/Storage/StorageUseCaseProtocol.swift
+++ FireFast/Src/Domain/Protocols/Storage/StorageUseCaseProtocol.swift
@@ -8,8 +8,7 @@
 import Foundation
 
 public protocol StorageUseCaseProtocol {
-  
-  func upload(data: Data, path: String, onSuccess: @escaping (UploadInfoModel) -> Void, onError: ((Error) -> Void)?)
+  func upload(data: Data, path: String, contentType: StorageContentType?, onSuccess: @escaping (UploadInfoModel) -> Void, progressCompleted: ((Double) -> Void)?, onError: ((Error) -> Void)?)
   func makeURL(path: String, onSuccess: @escaping (URL) -> Void, onError: ((Error) -> Void)?)
   func delete(path: String, completion: ((Error?) -> Void)?)
 }
