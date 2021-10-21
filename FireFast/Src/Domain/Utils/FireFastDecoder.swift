2af2ad4c025713bb9daa6cc040ce5b4719c6ec92
msoleimani <msoleimani@cognibox.com>
2021-10-20 23:22:59 -0400

    [UPDATE] tested date decoder sterategy

--- FireFast/Src/Domain/Utils/FireFastDecoder.swift
+++ FireFast/Src/Domain/Utils/FireFastDecoder.swift
@@ -10,7 +10,9 @@ import Foundation
 class FireFastDecoder: JSONDecoder {
   override func decode<T>(_ type: T.Type, from data: Data) throws -> T where T : Decodable {
     let decoder = JSONDecoder()
-    decoder.dateDecodingStrategy = .iso8601
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+    decoder.dateDecodingStrategy = .formatted(formatter)
     return try decoder.decode(T.self, from: data)
   }
 }
